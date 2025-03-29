d3dab32432c363770ea477015c10476f4e9b4d80
Jano-Guerra11 <janoguerra04@gmail.com>
2025-03-29 11:05:08 -0300

    .

---
+++ gestion de negocio/VistaProductos.aspx
@@ -0,0 +1,207 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="VistaProductos.aspx.cs" Inherits="gestion_de_negocio.VistaProductos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" href="estilos.css"/>
+    <link href="~/lib/twitter-bootstrap/css/bootstrap.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <title></title>
+    </head>
+<body id="bodyProductos">
+    <form id="form1" runat="server">
+        <main>
+                <div class="usuarioIniciado">
+                    <asp:Label ID="lblNegocioIniciado" runat="server" Text=""></asp:Label>
+                    <asp:Label ID="lblUsuarioIniciado" runat="server" Text=""></asp:Label>
+                </div>
+            <asp:Panel CssClass="contenedor_Productos" ID="pnlContenedor" runat="server">
+           <section class="agregarProducto">
+               <div class="contenedor_Imagen">
+                   <asp:FileUpload ID="flUpProducto" runat="server" />
+                   <asp:Button ID="btnGuardarImagen" runat="server" Text="Guardar imagen" OnClick="btnGuardarImagen_Click" />
+                   <asp:Image ID="imgProducto" runat="server" ImageUrl="" />
+               </div>
+
+    <table class="info_Producto">
+        <!-- -->
+        <tr class="fila">
+            <th class="table_header thNombre">Nombre</th>
+        </tr>
+        <tr class="fila">
+            <td><asp:TextBox CssClass="txtInfoProducto" ID="txtNombre" runat="server" placeholder="Nombre"></asp:TextBox></td>
+        </tr>
+        <!-- -->
+         <tr class="fila">
+            <th class="table_header">Seccion</th>
+         </tr>
+         <tr class="fila">
+             <td>
+                 <asp:DropDownList ID="ddlSecciones" runat="server">
+                     <asp:ListItem Value="0">- ninguna -</asp:ListItem>
+                 </asp:DropDownList>
+                 <asp:Button ID="btnAgregarSeccion" runat="server" Text="+" Font-Bold="True" Font-Size="Large" OnClick="btnAgregarSeccion_Click" />
+                 <asp:TextBox ID="txtNuevaSeccion" runat="server" Visible="false"></asp:TextBox>
+                 <asp:Label ID="lblMensajeAltaSeccion" runat="server" Text=""></asp:Label>
+             </td>
+         </tr>
+        <!-- 
+         <tr class="fila">
+            <th class="table_header">categoria/Modelo</th>
+        </tr>
+        <tr class="fila">
+           <td><asp:TextBox CssClass="txtInfoProducto" ID="txtCategoria" runat="server">categoria/modelo del producto</asp:TextBox></td>
+        </tr>
+        <!-- -->
+        <tr class="fila">
+            <th  class="table_header">Descripcion</th>
+        </tr>
+        <tr class="fila">
+            <td><asp:TextBox CssClass="txtInfoProducto" ID="txtDescripcion" runat="server" placeholder="Descripcion del producto"></asp:TextBox></td>
+        </tr>
+        <!-- -->
+        <tr class="fila">
+            <th  class="table_header">Precio</th>
+        </tr>
+        <tr class="fila">
+            <td><asp:TextBox CssClass="txtInfoProducto" ID="txtPrecio" runat="server" placeholder="0.0$"></asp:TextBox></td>
+        </tr>
+        <!-- -->
+        <tr class="fila">
+            <th  class="table_header">Stock</th>
+        </tr>
+        <tr class="fila">
+            <td><asp:TextBox CssClass="txtInfoProducto" ID="txtStock" runat="server" placeholder="20 U"></asp:TextBox></td>
+        </tr>
+        <!-- -->
+         <!-- -->
+        <tr class="fila">
+            <th  class="table_header">Proveedor</th>
+        </tr>
+        <tr class="fila">
+            <td>
+                <asp:DropDownList ID="ddlProveedores" runat="server"></asp:DropDownList>
+            <div class="input-group input-group-sm mb-3">
+    <asp:TextBox CssClass="form-control" ID="txtNombreProv" runat="server" placeholder="Nombre" Visible="False"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvNombreProv" runat="server" ErrorMessage="*" ControlToValidate="txtNombreProv" Text="*"></asp:RequiredFieldValidator>
+    <asp:TextBox ID="txtRazonSocialProv" runat="server" placeholder="Razon social" Visible="False"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvRazonSocialProv" runat="server" ErrorMessage="*" ControlToValidate="txtRazonSocialProv" Text="*" ValidationGroup="altaProv"></asp:RequiredFieldValidator>
+    <asp:TextBox ID="txtTelefonoProv" runat="server" placeholder="Telefono" Visible="False"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvTelefonoProv" runat="server" ErrorMessage="*" Text="*" ValidationGroup="altaProv" ControlToValidate="txtTelefonoProv"></asp:RequiredFieldValidator>
+    <asp:TextBox ID="txtMailProv" runat="server" placeholder="mail (opcional)" Visible="False"></asp:TextBox>
+    <asp:Button CssClass="btn btn-outline-secondary" ID="btnAgregarProveedor" runat="server" Text="Agregar Proveedor" OnClick="btnAgregarProveedor_Click" />
+    <asp:Label ID="lblMensajeErrorAgregarProveedor" runat="server" Text=""></asp:Label>
+</div>
+                </td>
+        </tr>
+        <!-- -->
+        <tr>
+           <td> <asp:Button ID="btnGuardar" runat="server" Text="Guardar" OnClick="btnGuardar_Click"/>
+               <asp:Label ID="lblMensajeAltaObaja" runat="server" Text=""></asp:Label>
+           </td>
+        </tr>
+    </table>  
+</section>
+            <section class="Productos">
+             <asp:Button ID="btnEsconder" runat="server" Text="<" OnClick="btnEsconder_Click" />
+            <div class="listadoYfiltrado">
+                <div class="contenedor_filtrado">
+                <table class="filtrado">
+                    <tr class="filaFiltrado">
+                        <td class="primeraCelda">Codigo </td>
+                       <td class="celdaMedia"> <asp:DropDownList CssClass="ddls" ID="ddlOpCodigo" runat="server">
+                           <asp:ListItem Value="=">igual a</asp:ListItem>
+                           <asp:ListItem Value="&gt;">mayor a </asp:ListItem>
+                           <asp:ListItem Value="&lt;">menor a </asp:ListItem>
+                           </asp:DropDownList></td>
+                        <td class="ultimaCelda">
+                            <asp:TextBox ID="txtCodigo" runat="server"></asp:TextBox></td>
+                        <td class="primeraCelda">Nombre </td><td class="ultimaCelda">
+                            <asp:TextBox ID="txtNombreProducto" runat="server"></asp:TextBox></td>
+                        <td class="primeraCelda">Seccion </td><td class="ultimaCelda">
+                            <asp:DropDownList CssClass="ddls" ID="ddlSeccion" runat="server"></asp:DropDownList>
+                        
+                                         </td>
+                    </tr>
+                    <tr class="filaFiltrado">
+                        <td class="primeraCelda">Categoria/modelo/tipo</td><td class="ultimaCelda">
+                            <asp:DropDownList CssClass="ddls" ID="ddlCategorias" runat="server"></asp:DropDownList>
+                            <asp:TextBox ID="txtNuevoTipo" runat="server" placeholder="Escriba la nueva categoria" Visible="false"></asp:TextBox>
+                                                      </td>
+                        <td class="primeraCelda">Precio</td><td class="celdaMedia" >
+                            <asp:DropDownList CssClass="ddls" ID="ddlOpPrecio" runat="server">
+                                <asp:ListItem  Value="=">Igual a</asp:ListItem>
+                                <asp:ListItem  Value="&gt;">Mayor a</asp:ListItem>
+                                <asp:ListItem  Value="&lt;">Menor a</asp:ListItem>
+                            </asp:DropDownList></td>
+                        <td class="ultimaCelda">
+                            <asp:TextBox ID="txtFiltroPrecio" runat="server"></asp:TextBox></td>
+                        <td class="primeraCelda">Stock</td><td class="celdaMedia">
+                            <asp:DropDownList CssClass="ddls" ID="ddlOpStock" runat="server">
+                                <asp:ListItem Value="=">Igual a </asp:ListItem>
+                                <asp:ListItem Value="&gt;">Mayor a</asp:ListItem>
+                                <asp:ListItem Value="&lt;">Menor a</asp:ListItem>
+                            </asp:DropDownList></td>
+                        <td class="ultimaCelda">
+                            <asp:TextBox ID="txtFiltroStock" runat="server"></asp:TextBox></td>
+                    </tr>
+                    <tr class="filaFiltrado">
+                        <td class="primeraCelda">Proveedor</td>
+                        <td class="celdaMedia">
+                            <asp:TextBox ID="txtFiltroProveedor" runat="server"></asp:TextBox></td>
+                        <td class="ultimaCelda"><asp:Button class="" ID="btnFiltrar" runat="server" Text="Filtrar Resultados" /></td>
+
+                    </tr>
+                </table>
+                </div>
+                <div class="contenedor_gridProductos">
+                <asp:GridView ID="grdProductos" runat="server" AutoGenerateColumns="False" AutoGenerateSelectButton="True" CssClass="GridView" OnSelectedIndexChanged="grdProductos_SelectedIndexChanged" OnDataBound="grdProductos_DataBound" OnRowDataBound="grdProductos_RowDataBound">
+                    <Columns>
+                        <asp:TemplateField HeaderText="Codigo">
+                            <ItemTemplate>
+                                <asp:Label ID="lbl_it_codigo" runat="server" Text='<%# Bind("idProducto_pr") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Nombre">
+                            <ItemTemplate>
+                                <asp:Label ID="lbl_it_nombre" runat="server" Text='<%# Bind("nombre_pr") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Seccion">
+                            <ItemTemplate>
+                                <asp:Label ID="lbl_it_seccion" runat="server" Text='<%# Bind("nombre_sec") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Descripcion">
+                            <ItemTemplate>
+                                <asp:Label ID="lbl_it_descripcion" runat="server" Text='<%# Bind("descripcion_pr") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Precio">
+                            <ItemTemplate>
+                                <asp:Label ID="lbl_it_precio" runat="server" Text='<%# Bind("precio_pr") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Stock">
+                            <ItemTemplate>
+                                <asp:Label ID="lbl_it_stock" runat="server" Text='<%# Bind("stock_pr") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Proveedor">
+                            <ItemTemplate>
+                                <asp:Label ID="lbl_it_proveedor" runat="server" Text='<%# Bind("nombre_prov") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+                </div>
+            </div>
+            </section>
+            </asp:Panel>
+        </main>
+    </form>
+    
+</body>
+</html>
---
+++ gestion de negocio/VistaProductos.aspx.cs
@@ -0,0 +1,308 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Entidades;
+
+namespace gestion_de_negocio
+{
+    public partial class VistaProductos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                validarUsuario();
+                fileUpload();
+                cargarGridProductos();
+                cargarDdlProveedores("0");
+                cargarDdlSecciones("0");
+            }
+        }
+        public void validarUsuario()
+        {
+            if (!validarCookiesUsuarioRecordado() &&
+                !validarSessionsIniciadoNoRecordado())
+            {
+                //usuario no inicado ni recordado
+                Response.Redirect("login.aspx");
+            }
+        }
+        private bool validarCookiesUsuarioRecordado()
+        {
+            bool recordado = false;
+            if (Request.Cookies["nombreUsuario"] != null &&
+                Request.Cookies["ContrasenaUsuario"] != null &&
+                Request.Cookies["negocio"] != null)
+            {
+                string nombreUsuario = Request.Cookies["nombreUsuario"].Value;
+                string nombreNegocio = Request.Cookies["negocio"].Value;
+
+                lblUsuarioIniciado.Text = "Usuario: " + nombreUsuario;
+                lblNegocioIniciado.Text = "Negocio: " + nombreNegocio;
+                Session["nombreNegocio"] = nombreNegocio;
+                recordado = true;
+            }
+            return recordado;
+        }
+        private bool validarSessionsIniciadoNoRecordado()
+        {
+            bool iniciadoNoRecordado = false;
+            if (Session["nombreUsuario"] != null && Session["nombreNegocio"] != null)
+            {
+                string UsuarioIniciado = Session["nombreUsuario"].ToString();
+                string NegocioIniciado = Session["nombreNegocio"].ToString();
+
+                lblUsuarioIniciado.Text = "Usuario: " + UsuarioIniciado;
+                lblNegocioIniciado.Text = "Negocio: " + NegocioIniciado;
+                iniciadoNoRecordado = true;
+            }
+            return iniciadoNoRecordado;
+        }
+        public void fileUpload()
+        {
+
+            if (imgProducto.ImageUrl == "")
+            {
+                flUpProducto.Visible = true;
+                btnGuardarImagen.Visible = true;
+                imgProducto.Visible = false;
+            }
+            else { flUpProducto.Visible = false;
+                btnGuardarImagen.Visible = false;
+                imgProducto.Visible = true;
+            }
+        }
+        public void cargarDdlProveedores(string idProvSeleccionado)
+        {
+
+            NegocioProveedores negProv = new NegocioProveedores();
+            DataTable proveedores = negProv.obtenerTodosLosProveedores();
+            ddlProveedores.Items.Clear();
+            ddlProveedores.DataSource = proveedores;
+            ddlProveedores.DataTextField = "nombre_prov";
+            ddlProveedores.DataValueField = "idProveedor_prov";
+            ddlProveedores.DataBind();
+            ddlProveedores.SelectedValue = idProvSeleccionado;// tiene que ser el proveedor asignado
+        }
+        public void cargarDdlSecciones(string idSeccionSeleccionada)
+        {
+            NegocioSecciones negSec = new NegocioSecciones();
+           int idNegocio = obtenerIdNegocioIniciado();
+            DataTable secciones = negSec.obtenerTablaSecciones(idNegocio);
+            ddlSecciones.Items.Clear();
+            ddlSecciones.DataSource = secciones;
+            ddlSecciones.DataTextField = "nombre_sec";
+            ddlSecciones.DataValueField = "idSeccion_sec";
+            ddlSecciones.DataBind();
+            ddlSecciones.SelectedValue = idSeccionSeleccionada;
+        }
+        private int obtenerIdNegocioIniciado()
+        {
+            NegocioNegocios Neg = new NegocioNegocios();
+            return Neg.obtenerID(Session["nombreNegocio"].ToString());
+        }
+        public void cargarGridProductos()
+        {
+            string nombreNegocio = Session["nombreNegocio"].ToString();
+            NegocioProductos negProd = new NegocioProductos();
+            grdProductos.DataSource = negProd.obtenerTablaProductosDeUnNegocio(nombreNegocio);
+            grdProductos.DataBind();
+        }
+        protected void btnEsconder_Click(object sender, EventArgs e)
+        {
+            mostrarYesconderAlta();
+        }
+        public void mostrarYesconderAlta()
+        {
+            if (btnEsconder.Text == "<")
+            {
+                pnlContenedor.CssClass = "contenedor_Productos-esconderAlta";
+                btnEsconder.Text = ">";
+
+            }
+            else
+            {
+                pnlContenedor.CssClass = "contenedor_Productos";
+                btnEsconder.Text = "<";
+            }
+        }
+      //---------------------------------------------------------------------------------------
+        protected void btnGuardarImagen_Click(object sender, EventArgs e)
+        {
+            if (flUpProducto.HasFile)
+            {
+                string extension = obtenerExtensionDelArchivo();
+                
+                if (extensionValida(extension))
+                {
+                    crearYguardarRutaDeLaImagen();
+                    cargarImagen();
+                }
+            }
+        }
+        private string obtenerExtensionDelArchivo()
+        {
+            return Path.GetExtension(flUpProducto.FileName).ToLower();
+        }
+        private bool extensionValida(string extension)
+        {
+            string[] extensionesValidas = { ".jpg", ".jpeg", ".png", ".gif" };
+            bool valida = extensionesValidas.Any(ex => ex == extension);
+            return valida;
+        }
+        private void crearYguardarRutaDeLaImagen()
+        {
+            // Creo la ruta absoluta en el servidor usando Server.MapPath
+            string carpetaDestino = Server.MapPath("~/imagenes/"); // Ruta física completa
+
+            string rutaCompleta = Path.Combine(carpetaDestino, flUpProducto.FileName);
+            flUpProducto.SaveAs(rutaCompleta);
+        }
+        private void cargarImagen()
+        {
+            imgProducto.ImageUrl = "/imagenes/" + flUpProducto.FileName;
+            fileUpload();
+        }
+       //----------------------------------------------------------------------------------------
+        protected void btnAgregarSeccion_Click(object sender, EventArgs e)
+        {
+            if(btnAgregarSeccion.Text == "+")
+            {
+                btnAgregarSeccion.Text = "Agregar";
+                txtNuevaSeccion.Visible = true;
+                lblMensajeAltaSeccion.Text = "";
+            }
+            else
+            {
+                btnAgregarSeccion.Text = "+";
+                txtNuevaSeccion.Visible= false;
+
+                bool alta = agregarNuevaSeccion();
+                procesarResultadoDeAltaSeccion(alta);
+            }
+        }
+        private bool agregarNuevaSeccion()
+        {
+            NegocioSecciones negSec = new NegocioSecciones();
+            NegocioNegocios neg = new NegocioNegocios();
+            int idNegocio = obtenerIdNegocioIniciado();
+            return negSec.altaSeccion(txtNuevaSeccion.Text, idNegocio);
+        }
+        private void procesarResultadoDeAltaSeccion(bool resultadoDeLaAccion)
+        {
+            NegocioSecciones negSec = new NegocioSecciones();
+            if(resultadoDeLaAccion)
+            {
+                lblMensajeAltaSeccion.Text = "Agregada";
+                string idNuevaSec = negSec.obtenerIdSeccion(txtNuevaSeccion.Text).ToString();
+                cargarDdlSecciones(idNuevaSec);
+            }
+            else lblMensajeAltaSeccion.Text = "No se pudo agregar";
+
+        }
+
+      //---------------------------------------------------------------------------------------
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            NegocioNegocios negneg = new NegocioNegocios();
+            NegocioProductos negProd = new NegocioProductos();
+            NegocioProdXProv negProdXProv = new NegocioProdXProv();
+           
+            int idProducto = negProd.obtenerIdDelProducto(txtNombre.Text);
+            bool accionExitosa = false;
+            int accionRealizada = 0;
+            int idNegocioIniciado = obtenerIdNegocioIniciado();
+          
+            int.TryParse(ddlSecciones.SelectedValue, out int idSeccion);
+            int.TryParse(txtPrecio.Text, out int precio);
+            int.TryParse(txtStock.Text, out int stock);
+            int.TryParse(ddlProveedores.SelectedValue, out int idProveedor);
+            
+            // primero cargo el objeto y despues verifico si existe o no 
+            if (idProducto == -1)
+            {
+                
+                accionExitosa = negProd.altaProducto(txtNombre.Text, idSeccion,
+                    txtDescripcion.Text,precio, stock, imgProducto.ImageUrl,idNegocioIniciado);
+
+                int nuevoIdProducto = negProd.obtenerIdDelProducto(txtNombre.Text);
+                accionExitosa = negProdXProv.altaProductoXProveedor(idProveedor,nuevoIdProducto,idNegocioIniciado);
+                
+                   accionRealizada = accionExitosa? 1 : 0;
+            }
+            else
+            {
+                accionExitosa = negProd.modificarProducto(txtNombre.Text, idSeccion,
+                    txtDescripcion.Text,precio, stock, imgProducto.ImageUrl);
+                //if(idProveedor != )
+                // modificar producto xproveedor tambien 
+                
+                
+                   accionRealizada = accionExitosa? 2 : 0;  
+            }
+
+            switch (accionRealizada)
+            {
+                case 1:
+                    lblMensajeAltaObaja.Text = "Producto agregado correctamente";
+                    break;
+                case 2:
+                    lblMensajeAltaObaja.Text = "Producto modificado correctamente";
+                    break;
+                default:
+                    lblMensajeAltaObaja.Text = "ERROR no se pudo completar la acción";
+                    break;
+            }
+            cargarGridProductos();
+
+        }
+        private Productos crearProductoRegistrado()
+        {
+            Productos prodNuevo = new Productos();
+            NegocioProductos negProd = new NegocioProductos();
+            prodNuevo.IdProducto_pr = negProd.obtenerIdDelProducto(txtNombre.Text);
+
+            
+        }
+      // ----------------------------------------------------------------------------------------
+        protected void btnAgregarProveedor_Click(object sender, EventArgs e)
+        {
+            if(btnAgregarProveedor.Text == "Agregar Proveedor")
+            {
+                btnAgregarProveedor.Text = "Guardar Proveedor";
+                txtNombreProv.Visible = true;
+                txtRazonSocialProv.Visible = true;
+                txtTelefonoProv.Visible = true;
+                txtMailProv.Visible = true;
+            }
+            else
+            {
+                NegocioProveedores negProv = new NegocioProveedores();
+                NegocioNegocios neNeg = new NegocioNegocios();
+                int idNegocio = neNeg.obtenerID(Session["nombreNegocio"].ToString());
+
+                if (negProv.altaProveedor(idNegocio,txtNombreProv.Text,txtRazonSocialProv.Text,
+                    txtTelefonoProv.Text,txtMailProv.Text))
+                {
+                    btnAgregarProveedor.Text = "Agregar Proveedor";
+                    txtNombreProv.Visible = false;
+                    txtRazonSocialProv.Visible = false;
+                    txtTelefonoProv.Visible = false;
+                    txtMailProv.Visible = false;
+                    lblMensajeErrorAgregarProveedor.Text = "Proveedor cargado";
+                }
+                else { lblMensajeErrorAgregarProveedor.Text = "Error"; }
+            }
+                    
+        }
+    }
+    
+}
